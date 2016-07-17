73397d6bdde48f5b5a92527b031c9011374fabab
fallen90 <slytherin9090@gmail.com>
2016-07-17 17:19:52 +0800

    removed outdated plugin

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'pos.ae');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '685362');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
